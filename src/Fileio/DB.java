eed4ad1ac56a5cae6de900a920d24147e5e618bd
TonyZXY <xyzheng109@gmail.com>
2016-09-28 21:01:59 +1000

    add new GUI menu

--- src/Fileio/DB.java
+++ src/Fileio/DB.java
@@ -1,10 +1,7 @@
 package Fileio;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Calendar;
 
 import org.hsqldb.Server;
 
@@ -13,7 +10,7 @@ import org.hsqldb.Server;
  * Created by TonyZheng on 27/9/16.
  */
 public class DB {
-    public static void main(String[] args) {
+    public static void startDB() {
         Server hsqlServer = null;
         Connection connection = null;
         ResultSet rs = null;
@@ -30,12 +27,66 @@ public class DB {
 //            connection.prepareStatement("DROP table barcodes if EXISTS ,").execute();
             connection.prepareStatement("CREATE table station (name VARCHAR (20), zone INTEGER );").execute();
             connection.prepareStatement("CREATE TABLE users (userID VARCHAR (20), userName VARCHAR (40),balance double, email VARCHAR (50),type CHAR,PRIMARY key (userID));").execute();
-            connection.prepareStatement("CREATE TABLE travelpass(passid VARCHAR (20),zone INTEGER ,price DOUBLE ,type char,time DATA ,PRIMARY key (passid));").execute();
+            connection.prepareStatement("CREATE TABLE travelpass(passid VARCHAR (20),zone INTEGER ,price DOUBLE ,duration char,type char,time DATA ,PRIMARY key (passid));").execute();
             connection.prepareStatement("CREATE TABLE history(passid VARCHAR (20),userid VARCHAR (20),PRIMARY key (passid,userid),FOREIGN KEY (passid) REFERENCES travelpass(passid),FOREIGN KEY (userid) REFERENCES users(userid))").execute();
+            Statement insertStation = connection.createStatement();
+            insertStation.addBatch("insert into station values ('Central', 1);");
+            insertStation.addBatch("insert into station values ('Flagstaff', 1);");
+            insertStation.addBatch("INSERT into station values ('Richmond',1)");
+            insertStation.addBatch("insert into station values ('Lilydale',2)");
+            insertStation.addBatch("insert into station values ('Epping',2)");
+            insertStation.executeBatch();
+            connection.prepareStatement("insert into users values ('lc','Lawrence Cavedon',0,'lawrence.cavedon@rmit.edu.au','A');").execute();
+            connection.prepareStatement("insert into users values ('abc','abcde',40.0,'abcdefg','C');").execute();
+
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e2) {
             e2.printStackTrace();
+        }
+    }
+
+    int travelPassID = 10000000;
+
+    public void addTravelPassDB(String usersID, int zone, char type, char duration, Calendar date,double price){
+        try{
+            Connection addTrivel = DriverManager.getConnection("jdbc:hsqldb:TestDB","sa","123");
+            String statement = "insert into travelpass values ( '"+travelPassID+"','"+zone+"','"+price+"','"+duration+"','"+type+"','"+date.getTime()+"');";
+            addTrivel.prepareStatement(statement).execute();
+            String statment2 = "insert into history values ('"+travelPassID+"','"+usersID+"');";
+            addTrivel.prepareStatement(statment2).execute();
+            travelPassID++;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addNewUserDB(String userID,String userName,double balance,String email,char type){
+        try{
+            Connection addUser = DriverManager.getConnection("jdbc:hsqldb:TestDB","sa","123");
+            String statement = "insert into users ( '"+userID+"','"+userName+"','"+balance+"','"+email+"','"+type+"';)";
+            addUser.prepareStatement(statement).execute();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addNewUserDB(String userID,String userName,String email,char type){
+        try{
+            Connection addUser = DriverManager.getConnection("jdbc:hsqldb:TestDB","sa","123");
+            String statement = "insert into users('"+userID+"','"+userName+"','"+0+"','"+email+"','"+type+"';)";
+            addUser.prepareStatement(statement).execute();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void getUserBalanceDB(String userID){
+        try{
+            Connection getBalance = DriverManager.getConnection("jdbc:hsqldb:TestDB","sa","123");
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
---
+++ src/GUI/TopUpMenu/Controller.java
@@ -0,0 +1,9 @@
+package GUI.TopUpMenu;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+}
